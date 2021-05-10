3a68dfeab3530c0383c8d123170732517da9d1cd
Mohammed Rasik <rasikmhd10@gmail.com>
2021-05-10 16:41:47 +0530

    port change

--- config.py
+++ config.py
@@ -3,18 +3,14 @@ class Config(object):
     MONGO_URL = "mongodb://localhost:27017/"
 
 class DevelopmentConfig(Config):
-    API_PORT = 9901
     DEBUG = True
 
 class TestingConfig(Config):
-    API_PORT = 9902
     TESTING = True
 
 class StagingConfig(Config):
-    API_PORT = 9903
     TESTING = True
 
 class ProductionConfig(Config):
-    API_PORT = 9904
     DEBUG = False
     TESTING = False
