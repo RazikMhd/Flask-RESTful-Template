0ba0db50ff71d28655066a44807508eca5994e7f
Mohammed Rasik <rasikmhd10@gmail.com>
2021-05-16 19:39:39 +0530

    Flask CORS added

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
